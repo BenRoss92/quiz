8bcd368cba76a2316da2f8dfc4b22fd34fde6c5c
BenRoss92 <benjaminjackross@gmail.com>
2017-02-23 16:25:31 +0000

    Fix feature test for ending quiz

--- spec/features/ending_quiz_spec.rb
+++ spec/features/ending_quiz_spec.rb
@@ -1,5 +1,9 @@
 feature 'ending the quiz' do
+  let(:question_list) { QuestionList.instance }
+
   scenario 'an ending page is displayed after answering last question' do
+    allow(question_list).to receive(:select_question).with(3).and_raise(
+    Exceptions::NoQuestionError)
     visit('/questions/3')
     expect(page).to have_current_path('/end')
     expect(page).to have_content("That's it!")
