2ee1733f115ee57af6bdb0dd8ba2181f6cc7dacf
BenRoss92 <benjaminjackross@gmail.com>
2017-02-23 16:32:33 +0000

    Stub questions in feature tests - skip & view questions

--- spec/features/skipping_questions_spec.rb
+++ spec/features/skipping_questions_spec.rb
@@ -1,5 +1,25 @@
 feature 'triggering next question' do
+  let(:question_list) { QuestionList.instance }
+  let(:data) do
+    [{
+      time_limit: 10,
+      text: "Who was the legendary Benedictine monk who invented champagne?",
+      correct_answer: "Dom Perignon",
+      option_2: "Ansgar",
+      option_3: "Willibrord"
+      },
+      {
+        time_limit: 12,
+        text: "Name the largest freshwater lake in the world?",
+        correct_answer: "Lake Superior",
+        option_2: "Lake Victoria",
+        option_3: "Lake Huron"
+      }]
+    end
+
   scenario 'user can trigger next question manually' do
+    allow(question_list).to receive(:questions).and_return(data)
+    
     visit('/')
     click_link('Start')
     expect(page).to have_current_path('/questions/1')
--- spec/features/viewing_question_spec.rb
+++ spec/features/viewing_question_spec.rb
@@ -1,5 +1,25 @@
 feature 'viewing a question' do
+  let(:question_list) { QuestionList.instance }
+  let(:data) do
+    [{
+      time_limit: 10,
+      text: "Who was the legendary Benedictine monk who invented champagne?",
+      correct_answer: "Dom Perignon",
+      option_2: "Ansgar",
+      option_3: "Willibrord"
+      },
+      {
+        time_limit: 12,
+        text: "Name the largest freshwater lake in the world?",
+        correct_answer: "Lake Superior",
+        option_2: "Lake Victoria",
+        option_3: "Lake Huron"
+      }]
+  end
+
   scenario 'a user sees only one question at a time' do
+    allow(question_list).to receive(:questions).and_return(data)
+    
     visit('/')
     click_link('Start')
     expect(page).to have_current_path('/questions/1')
