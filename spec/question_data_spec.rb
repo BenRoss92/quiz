53c19f8b1fc0f1d03d787c6a617f7f55ebf2ce6a
BenRoss92 <benjaminjackross@gmail.com>
2017-02-24 15:17:37 +0000

    Gemfile updated with Ruby version + '.load_questions' test failing

--- spec/question_data_spec.rb
+++ spec/question_data_spec.rb
@@ -3,6 +3,8 @@ require './app/models/question_data'
 describe QuestionData do
 
   subject(:question_data_instance) { described_class.new(data) }
+  let(:question_data_class) { double(:question_data_class, new: question_data_instance) }
+  let(:file) { double(:file) }
   let(:data) do
     [{
       time_limit: 10,
@@ -22,6 +24,14 @@ describe QuestionData do
 
   it "initializes with an array of questions" do
     expect(question_data_instance.questions).to eq(data)
+  end
+
+  describe '.load_questions' do
+    it "creates an instance containing CSV data" do
+      allow(question_data_class).to receive(:load_questions).with(file)
+      expect(question_data_class).to receive(:new).with(data).and_return(question_data_instance)
+      question_data_class.load_questions(file)
+    end
   end
 
 end
