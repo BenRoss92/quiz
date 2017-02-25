36c604a37df7603c0c3459c4e04975b1015ec25c
BenRoss92 <benjaminjackross@gmail.com>
2017-02-25 00:17:49 +0000

    Rewrite QuestionData.load_questions method - Question Data needs testing

--- app/models/question_data.rb
+++ app/models/question_data.rb
@@ -11,15 +11,15 @@ class QuestionData
   end
 
   def self.load_questions(file = './app/lib/question_list.csv')
-    @questions = []
+    questions = []
     CSV.foreach(file) do |row|
       time_limit, text, correct_answer, option_2, option_3 = row[0],
        row[1], row[2], row[3], row[4]
-       @questions << { time_limit: time_limit, text: text,
+      questions << { time_limit: time_limit, text: text,
         correct_answer: correct_answer, option_2: option_2, option_3: option_3
       }
     end
-    self.new(@questions)
+    self.new(questions)
   end
 
 end
--- spec/question_data_spec.rb
+++ spec/question_data_spec.rb
@@ -2,36 +2,4 @@ require './app/models/question_data'
 
 describe QuestionData do
 
-  subject(:question_data_instance) { described_class.new(data) }
-  let(:question_data_class) { double(:question_data_class, new: question_data_instance) }
-  let(:file) { double(:file) }
-  let(:data) do
-    [{
-      time_limit: 10,
-      text: "Who was the legendary Benedictine monk who invented champagne?",
-      correct_answer: "Dom Perignon",
-      option_2: "Ansgar",
-      option_3: "Willibrord"
-      },
-      {
-        time_limit: 12,
-        text: "Name the largest freshwater lake in the world?",
-        correct_answer: "Lake Superior",
-        option_2: "Lake Victoria",
-        option_3: "Lake Huron"
-      }]
-  end
-
-  it "initializes with an array of questions" do
-    expect(question_data_instance.questions).to eq(data)
-  end
-
-  describe '.load_questions' do
-    it "creates an instance containing CSV data" do
-      allow(question_data_class).to receive(:load_questions).with(file)
-      expect(question_data_class).to receive(:new).with(data).and_return(question_data_instance)
-      question_data_class.load_questions(file)
-    end
-  end
-
 end
