5e2d49a156eb6ce03b32eda11fbc4b91ff43dbea
BenRoss92 <benjaminjackross@gmail.com>
2017-03-25 00:01:47 +0000

    Remove Ruby question validation tests & add accessor tests

--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -16,35 +16,32 @@ describe Question do
   let(:invalid_answer) { "Ansgar" }
   let(:shuffled_options) { ["Dom Perignon", "Ansgar", "Willibrord"] }
 
+  it "knows its time_limit" do
+    expect(question.time_limit).to eq(10)
+  end
+
   it "knows its text" do
     expect(question.text).to eq(
     "Who was the legendary Benedictine monk who invented champagne?"
     )
   end
 
-  it "knows its time_limit" do
-    expect(question.time_limit).to eq(10)
+  it "knows its correct answer" do
+    expect(question.correct_answer).to eq("Dom Perignon")
+  end
+
+  it "knows its second answer option" do
+    expect(question.option_2).to eq("Ansgar")
+  end
+
+  it "knows its third answer option" do
+    expect(question.option_3).to eq("Willibrord")
   end
 
   describe '#shuffle_options' do
     it "shuffles the answer options" do
       allow_any_instance_of(Array).to receive(:shuffle).and_return(shuffled_options)
       expect(question.shuffle_options).to eq(shuffled_options)
-    end
-  end
-
-  describe '#get_result' do
-    context 'when answer is incorect' do
-      it "returns the incorrect verdict and the correct answer" do
-        expect(question.get_result(invalid_answer)).to include(
-        verdict: :incorrect, correct: valid_answer)
-      end
-    end
-
-    context 'when answer is correct' do
-      it "shows the verdict to be correct" do
-        expect(question.get_result(valid_answer)).to include(verdict: :correct)
-      end
     end
   end
 
