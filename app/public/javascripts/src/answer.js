3be44b4cf40f11c7783d6e819c0567f8ec9a242f
BenRoss92 <benjaminjackross@gmail.com>
2017-04-14 17:17:49 +0100

    Submission removed from Answer initialization

--- app/public/javascripts/src/answer.js
+++ app/public/javascripts/src/answer.js
@@ -1,11 +1,11 @@
 /*jslint node: true */
 "use strict";
 
-function Answer(submission, questionObject) {
-  this.submission = submission;
-  this._correctAnswer = questionObject.correct_answer;
+function Answer(questionData) {
+  this._questionData = questionData;
+  this._correctAnswer = questionData.correct_answer;
 }
 
-Answer.prototype.validate = function () {
-  return (this.submission === this._correctAnswer) ? true : false;
+Answer.prototype.validate = function (submission) {
+  return (this._correctAnswer === submission) ? true : false;
 };
--- spec/javascripts/answerSpec.js
+++ spec/javascripts/answerSpec.js
@@ -1,51 +1,31 @@
 describe("Answer", function() {
 
-  var submission;
+  var correctSubmission;
+  var incorrectSubmission;
   var answer;
-  var questionObject;
+  var questionData;
 
-  describe("When submission is incorrect", function() {
-
-    beforeEach(function() {
-      submission = 'Willibrord';
-      questionObject = {
-        "time_limit":"10",
-        "text":"Who was the legendary Benedictine monk who invented champagne?",
-        "correct_answer":"Dom Perignon",
-        "option_2":"Ansgar",
-        "option_3":"Willibrord"
-      };
-      answer = new Answer(submission, questionObject);
-    });
-
-    it("has a submission", function() {
-      expect(answer.submission).toEqual('Willibrord');
-    });
-
-    it("validates a user's submission", function() {
-      expect(answer.validate()).toEqual(false);
-    });
-
+  beforeEach(function() {
+    questionData = {
+      "time_limit":"10",
+      "text":"Who was the legendary Benedictine monk who invented champagne?",
+      "correct_answer":"Dom Perignon",
+      "option_2":"Ansgar",
+      "option_3":"Willibrord"
+    };
+    answer = new Answer(questionData);
+    correctSubmission = 'Dom Perignon';
+    incorrectSubmission = 'Willibrord';
   });
 
-  describe("When submission is correct", function() {
-
-    beforeEach(function() {
-      submission = 'Dom Perignon';
-      questionObject = {
-        "time_limit":"10",
-        "text":"Who was the legendary Benedictine monk who invented champagne?",
-        "correct_answer":"Dom Perignon",
-        "option_2":"Ansgar",
-        "option_3":"Willibrord"
-      };
-      answer = new Answer(submission, questionObject);
+  describe("#validate", function() {
+    it("correct submission validates as true", function() {
+      expect(answer.validate(correctSubmission)).toEqual(true);
     });
 
-    it("validates as true", function() {
-      expect(answer.validate()).toEqual(true);
+    it("incorrect submission validates as false", function() {
+      expect(answer.validate(incorrectSubmission)).toEqual(false);
     });
-    
   });
 
 });
