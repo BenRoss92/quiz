112b813a46a6dea4f54ae95ae8499a14becc3999
BenRoss92 <benjaminjackross@gmail.com>
2017-04-17 22:29:20 +0100

    Refactor Timer tests

--- spec/javascripts/timerSpec.js
+++ spec/javascripts/timerSpec.js
@@ -1,38 +1,46 @@
 describe("Timer", function() {
 
-  var timer;
-  var startTime;
+  var positiveTimer;
+  var zeroTimer;
+  var positiveSeconds;
+  var zeroSeconds;
   var count;
 
-  describe("When timer is set to 10", function() {
+  beforeEach(function() {
+    positiveSeconds = 10;
+    zeroSeconds = 0;
+    positiveTimer = new Timer(positiveSeconds);
+    zeroTimer = new Timer(zeroSeconds);
+  });
 
-    beforeEach(function() {
-      startTime = 10;
-      timer = new Timer(startTime);
-    });
+  describe("#getRemaining", function() {
 
     it("returns the remaining seconds", function() {
-      expect(timer.getRemaining()).toEqual(startTime);
-    });
-
-    it("reduces remaining seconds by one", function() {
-      timer.reduceTime();
-      expect(timer.getRemaining()).toEqual(startTime - 1);
+      expect(positiveTimer.getRemaining()).toEqual(positiveSeconds);
     });
 
   });
 
-  describe("When starting time is hardcoded as 0", function() {
+  describe("#reduceTime", function() {
 
-    beforeEach(function() {
-      startTime = 0;
-      timer = new Timer(startTime);
+    describe("When timer is given a positive starting time", function() {
+
+      it("reduces remaining seconds by one", function() {
+        positiveTimer.reduceTime();
+        expect(positiveTimer.getRemaining()).toEqual(positiveSeconds - 1);
+      });
+
     });
 
-    it("throws error if time remaining goes below 0", function() {
-      expect(function(){ timer.reduceTime();}).toThrowError(
-        "remaining time cannot be less than 0");
+    describe("When timer has zero seconds remaining", function() {
+
+      it("throws error if time remaining goes below 0", function() {
+        expect(function(){ zeroTimer.reduceTime();}).toThrowError(
+          "remaining time cannot be less than 0");
+      });
+
     });
+
   });
 
 });
