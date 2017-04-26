78d67cec6b9b78b0a083278fc424a2533f4b2205
BenRoss92 <benjaminjackross@gmail.com>
2017-04-26 17:12:26 +0100

    Correct answer displays 'Correct' verdict - interface.js - view and controller functions needs separating - Deleted API model and tests for now - needs rebuilding

--- spec/features/submitting_answers_spec.rb
+++ spec/features/submitting_answers_spec.rb
@@ -1,5 +1,5 @@
-feature 'answering a question' do
-  scenario 'when answered correctly, displays correct verdict', js: true do
+feature 'submitting an answer' do
+  scenario 'when submission is correct, displays correct verdict', js: true do
     visit('/')
     click_link('Start')
     choose('Dom Perignon')
